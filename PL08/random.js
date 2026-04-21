7392756c58315a1cc891e272674210fd0ac20dec
Paula <paulaguollo00@gmail.com>
2026-04-21 16:56:28 +0100

    PL08

---
+++ PL08/random.js
@@ -0,0 +1,8 @@
+/*8- Num novo script escreva “console.log(Math.random());” verifique o resultado. Recarregue a página e verifique se 
+há diferença. Escreva uma linha/parágrafo sobre o resultado da operação executada. */
+
+function random(){
+    console.log(Math.random());
+}
+
+random()
